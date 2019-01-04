4a0ee1614382c0d1c4c3fd068992b0284b477c18
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2019-01-04 12:51:40 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+
+// array for cardsInPlay
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+	// var cardOne = cards[0];
+	// cardsInPlay.push(cardOne);
+
+	// var cardTwo = cards[2];
+	// cardsInPlay.push(cardTwo);
+
+
+
